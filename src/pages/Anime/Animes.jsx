61d7ca712538876ef60ca5333a12e12b9dd28cfb
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-04-29 20:36:37 +0530

    changes made

--- src/pages/Anime/Animes.jsx
+++ src/pages/Anime/Animes.jsx
@@ -1,22 +1,13 @@
-import { Context } from "../../context/Context"
-import React,{ useContext } from "react"
-import { Link } from "react-router-dom"
-import AnimeCard from "../../components/AnimeCard"
+import { Context } from "../../context/Context";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import AnimeCard from "../../components/AnimeCard";
 // import { useParams } from "react-router-dom"
 
-export default function Animes(){
+export default function Animes() {
+  const { animeData } = useContext(Context);
+  // console.log(animeData)
+  const map = animeData.map((e) => <AnimeCard key={e.mal_id} {...e} />);
 
-    
-
-    const {animeData} = useContext(Context)
-    // console.log(animeData)
-    const map = animeData.map(e => (
-        <AnimeCard key={e.mal_id} {...e}/>
-    ))
-    
-    return(
-        <main>
-            {map}
-        </main>
-    )
+  return <main>{map}</main>;
 }
--- src/pages/AnimeDetail/AnimeDetail.jsx
+++ src/pages/AnimeDetail/AnimeDetail.jsx
@@ -1,65 +1,65 @@
-import { Context } from "../../context/Context"
-import React,{ useContext,useEffect,useState } from "react"
-import { useParams } from 'react-router-dom'
-import Loader from "../../components/Loader"
-import styles from "../../css/animedetail.module.css"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../../components/Loader";
+import styles from "../../css/animedetail.module.css";
+import axios from "axios";
 
+export default function AnimeDetail() {
+  const [details, setDetails] = useState([]);
+  const { id } = useParams();
+  console.log(id);
 
-export default function AnimeDetail(){
-    const [details,setDetails] = useState([])
-    const {id} = useParams()
-    console.log(id)
+  const [loader, setLoader] = useState(true);
 
-   const [loader,setLoader] = useState(true)
-    
-    useEffect(()=>{
-      setTimeout(async()=>{
-        const response = await axios.get(`https://api.jikan.moe/v4/anime/${id}/full`)
-        setDetails(response.data.data)
-        setLoader(false)
-      })
-    },[id])
+  useEffect(() => {
+    setTimeout(async () => {
+      const response = await axios.get(
+        `https://api.jikan.moe/v4/anime/${id}/full`
+      );
+      setDetails(response.data.data);
+      setLoader(false);
+    });
+  }, [id]);
 
-    console.log(details)
-    
-    const {images,title,episodes,trailer,producers,score,synopsis,genres,mal_id} = details
-    
+  console.log(details);
+
+  const {
+    images,
+    title,
+    episodes,
+    trailer,
+    producers,
+    score,
+    synopsis,
+    genres,
+    mal_id,
+  } = details;
+
   return (
     <>
-    {loader && <Loader/>}
-    {details && 
-    <div className={styles.animecard} key={mal_id}>
-      {images && <img src={images.jpg.image_url} alt={title} />}
-      <h2>{title}</h2>
-      <div className={styles.score}>
-        <h3>Score: {score}</h3>
-        <h3>No. of episodes: {episodes}</h3>
-      </div>
-      <div className={styles.genre}>
-      {genres && genres.map(e => (
-        <h4>{e.name}</h4>
-      ))}
-      </div>
-      <h3>Producers</h3>
-      <div className={styles.genre}>
-        {producers && producers.map(e => (
-          <h4>{e.name}</h4>
-        ))}
-      </div>
-      
-      <p>{synopsis}</p>
-      {trailer 
-        &&
-        <iframe src={trailer.embed_url} frameborder="0"></iframe>
-      }
-      
-    </div>}
-    
-      
+      {loader && <Loader />}
+      {details && (
+        <div className={styles.animecard} key={mal_id}>
+          {images && <img src={images.jpg.image_url} alt={title} />}
+          <h2>{title}</h2>
+          <div className={styles.score}>
+            <h3>Score: {score}</h3>
+            <h3>No. of episodes: {episodes}</h3>
+          </div>
+          <div className={styles.genre}>
+            {genres && genres.map((e) => <h4>{e.name}</h4>)}
+          </div>
+          <h3>Producers</h3>
+          <div className={styles.genre}>
+            {producers && producers.map((e) => <h4>{e.name}</h4>)}
+          </div>
+
+          <p>{synopsis}</p>
+          {trailer && trailer.embed_url && (
+            <iframe src={trailer.embed_url} frameborder="0"></iframe>
+          )}
+        </div>
+      )}
     </>
-  )
+  );
 }
-
-
-
